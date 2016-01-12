b53cc5690e5f013e6eafea37ed192a5a021f9130
Chubuntu Arciniega <arciniega.chubuntu@gmail.com>
2016-01-12 10:36:56 -0700

    Plantilla
    Comienzo plantilla

--- dashboard/back.php
+++ dashboard/back.php
@@ -3,54 +3,11 @@
     Desarrollado en PHP - HTML5 - Javascript - CSS
     Utilizando Materialize http://materializecss.com/
     Derechos Reservados 2016-->
-<?php require '../php/master.php';?>
+<?php require '../php/master.php';
+require('../assets/dashboard/header.php');?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <!--Meta-Tags-->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width-device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta name="autor" content="Jesus Arciniega">
-    <meta name="description" content="Dashboard Corporativo de AIMCO Corporation de México" />
-    <link rel="shortcut icon" href="../img/favicon.ico" />
-    <!--/Meta-Tags-->
-    <title>AIMCO CORPORATION DE MÉXICO SA DE CV</title>
-    <!--Stylesheets-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css" media="screen" title="no title" charset="utf-8">
-    <link rel="stylesheet" href="../css/diseno.css" media="screen" title="no title" charset="utf-8">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!--/Stylesheets-->
-  </head>
   <body>
-    <!--Barra superior - Header-->
-    <header class="z-depth-1">
-        <ul>
-          <li><a class="activo" href="../dashboard">AIMCO</a></li>
-          <li><a href="http://www.aimco-solutions.com/acradyne.asp" target="_blank" id="ocultar">AcraDyne</a></li>
-          <li><a href="http://www.eagle-premier.com/" target="_blank" id="ocultar">Eagle</a></li>
-          <li><a href="http://www.aimco-solutions.com/online_catalog.asp" target="_blank" id="ocultar">Catalogos</a></li>
-          <li><a href="#" id="ocultar">Inventarios</a></li>
-          <li><a href="../dashboard/directorio.php" id="ocultar">Directorio</a></li>
-          <li><a href="../dashboard/actualiza_comedor.php" id="ocultar" <?php if($_SESSION['Nombre_Usuario'] != 'Veronica Murillo'){ echo "style='display: none;'";} ?>>Menú Comedor</a></li>
-          <li><a href="#" id="ocultar" <?php if($_SESSION['Rango'] < 4){ echo "style='display: none;'";} ?>>Comedor</a></li>
-          <li><a href="#" id="ocultar">Soporte</a></li>
-          <li><a><?php echo $_SESSION['Nombre_Usuario']; ?></a></li>
-          <li><i class="material-icons sesion" data-activates='dropdown1'>supervisor_account</i></li>
-          <!-- Dropdown Structure -->
-          <ul id='dropdown1' class='dropdown-content'>
-            <li><a class="modal-trigger" href="#modal3">Información</a></li>
-            <li class="divider"></li>
-            <li><a href="../">Cerrar Sesión</a></li>
-          </ul>
-        </ul>
-    </header>
-    <nav id="tiulo_pagina">
-      <div class="nav-wrapper gray darken-4 ">
-        <img src="../img/logo.png" id="logo">
-          <a href="../dashboard" id="aimco">AIMCO Corporation de México</a>
-      </div>
-    </nav>
-    <!--/Barra superior - Header-->
     <!--Gráfica-->
     <div class="row">
       <div class="col s12 m9" id="ocultar">
