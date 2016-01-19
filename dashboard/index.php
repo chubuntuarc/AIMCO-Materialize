ef565e2f875ca5939275f25dd4dab43e5ffa72de
Chubuntu Arciniega <arciniega.chubuntu@gmail.com>
2016-01-19 15:16:24 -0700

    Nuevas barras

--- dashboard/index.php
+++ dashboard/index.php
@@ -38,58 +38,56 @@ require('../assets/dashboard/header.php'); ?>
     <!--/Numeros Top-->
   <!--Gráfica-->
   <div class="row" <?php if($_SESSION['Rango'] != 3){ echo "style='display: none;'";} ?>>
-    <div class="col s12 m9" id="ocultar">
-      <div class="card-panel z-depth-3">
-        <h5 id="Titulo_Grafica">Facturas de Clientes</h5>
-          <canvas id="canvas" height="150" width="400"></canvas>
-          <!--Inputs ocultos para la consulta de los valores de las graficas-->
-          <!--El id usado en los input sirve para llevar la cadena al script JS y esconderlos usando styles/adicionales.css-->
-          <input type="text" id="campos_facturacion" value="
-            <?php
-                  $Consulta_Grafica_Facturas ="SELECT sum(T1.[TotalSumSy])AS Total FROM OINV T0  INNER JOIN INV1 T1 ON T0.DocEntry = T1.DocEntry INNER JOIN OSLP T2 ON T0.SlpCode = T2.SlpCode WHERE T0.[DocDate] >= '".$_SESSION["Anual"]."' AND  T2.[U_CODIGO_USA]  = ".$_SESSION['Usuario_Actual']." AND T1.[TargetType] <> 14 GROUP BY month(T0.[DocDate]) ORDER BY month(T0.[DocDate])";
-                $Resultado_Consulta_Grafica = odbc_exec($Conexion_SQL, $Consulta_Grafica_Facturas);
-                while ($fac = odbc_fetch_array($Resultado_Consulta_Grafica)) {
-                  foreach ($fac as $meses) {
-                    echo $meses.",";
-                  }
+    <div class="col m9 s12">
+        <div id="barra"></div>
+
+        <!--Inputs ocultos para la consulta de los valores de las graficas-->
+        <!--El id usado en los input sirve para llevar la cadena al script JS y esconderlos usando styles/adicionales.css-->
+        <input type="text" id="campos_facturacion" value="
+          <?php
+                $Consulta_Grafica_Facturas ="SELECT sum(T1.[TotalSumSy])AS Total FROM OINV T0  INNER JOIN INV1 T1 ON T0.DocEntry = T1.DocEntry INNER JOIN OSLP T2 ON T0.SlpCode = T2.SlpCode WHERE T0.[DocDate] >= '".$_SESSION["Anual"]."' AND  T2.[U_CODIGO_USA]  = ".$_SESSION['Usuario_Actual']." AND T1.[TargetType] <> 14 GROUP BY month(T0.[DocDate]) ORDER BY month(T0.[DocDate])";
+              $Resultado_Consulta_Grafica = odbc_exec($Conexion_SQL, $Consulta_Grafica_Facturas);
+              while ($fac = odbc_fetch_array($Resultado_Consulta_Grafica)) {
+                foreach ($fac as $meses) {
+                  echo $meses.",";
                 }
-            ?>
-          ">
-          <input type="text" id="campos_ordenes" value="
-            <?php
-                $Consulta_Grafica_Ordenes ="SELECT sum(T1.[TotalSumSy])AS Total FROM ORDR T0  INNER JOIN RDR1 T1 ON T0.DocEntry = T1.DocEntry INNER JOIN OSLP T2 ON T0.SlpCode = T2.SlpCode WHERE T0.[DocDate] >= '".$_SESSION["Anual"]."' AND T0.[CANCELED] = 'N' AND   T2.[U_CODIGO_USA]  = ".$_SESSION['Usuario_Actual']." GROUP BY month(T0.[DocDate]) ORDER BY month(T0.[DocDate])";
-                $Resultado_Consulta_Grafica_Ordenes = odbc_exec($Conexion_SQL, $Consulta_Grafica_Ordenes);
-                while ($ord = odbc_fetch_array($Resultado_Consulta_Grafica_Ordenes)) {
-                  foreach ($ord as $meses_ord) {  //Las variables en while y foreach de los input son solo para uso en ellos mismos.
-                    echo $meses_ord.",";
-                  }
+              }
+          ?>
+        ">
+        <input type="text" id="campos_ordenes" value="
+          <?php
+              $Consulta_Grafica_Ordenes ="SELECT sum(T1.[TotalSumSy])AS Total FROM ORDR T0  INNER JOIN RDR1 T1 ON T0.DocEntry = T1.DocEntry INNER JOIN OSLP T2 ON T0.SlpCode = T2.SlpCode WHERE T0.[DocDate] >= '".$_SESSION["Anual"]."' AND T0.[CANCELED] = 'N' AND   T2.[U_CODIGO_USA]  = ".$_SESSION['Usuario_Actual']." GROUP BY month(T0.[DocDate]) ORDER BY month(T0.[DocDate])";
+              $Resultado_Consulta_Grafica_Ordenes = odbc_exec($Conexion_SQL, $Consulta_Grafica_Ordenes);
+              while ($ord = odbc_fetch_array($Resultado_Consulta_Grafica_Ordenes)) {
+                foreach ($ord as $meses_ord) {  //Las variables en while y foreach de los input son solo para uso en ellos mismos.
+                  echo $meses_ord.",";
                 }
-            ?>
-          ">
-          <input type="text" id="campos_ofertas" value="
-            <?php
-                $Consulta_Grafica_Ofertas ="SELECT sum(T1.[TotalSumSy])AS Total FROM OQUT T0  INNER JOIN QUT1 T1 ON T0.DocEntry = T1.DocEntry INNER JOIN OSLP T2 ON T0.SlpCode = T2.SlpCode WHERE T0.[DocDate] >= '".$_SESSION["Anual"]."' AND T2.[U_CODIGO_USA]  = ".$_SESSION['Usuario_Actual']." GROUP BY month(T0.[DocDate]) ORDER BY month(T0.[DocDate])";
-                $Resultado_Consulta_Grafica_Ofertas = odbc_exec($Conexion_SQL, $Consulta_Grafica_Ofertas);
-                while ($ofe = odbc_fetch_array($Resultado_Consulta_Grafica_Ofertas)) {
-                  foreach ($ofe as $meses_ofe) {
-                    echo $meses_ofe.",";
-                  }
+              }
+          ?>
+        ">
+        <input type="text" id="campos_ofertas" value="
+          <?php
+              $Consulta_Grafica_Ofertas ="SELECT sum(T1.[TotalSumSy])AS Total FROM OQUT T0  INNER JOIN QUT1 T1 ON T0.DocEntry = T1.DocEntry INNER JOIN OSLP T2 ON T0.SlpCode = T2.SlpCode WHERE T0.[DocDate] >= '".$_SESSION["Anual"]."' AND T2.[U_CODIGO_USA]  = ".$_SESSION['Usuario_Actual']." GROUP BY month(T0.[DocDate]) ORDER BY month(T0.[DocDate])";
+              $Resultado_Consulta_Grafica_Ofertas = odbc_exec($Conexion_SQL, $Consulta_Grafica_Ofertas);
+              while ($ofe = odbc_fetch_array($Resultado_Consulta_Grafica_Ofertas)) {
+                foreach ($ofe as $meses_ofe) {
+                  echo $meses_ofe.",";
                 }
-            ?>
-          ">
-          <input type="text" id="campos_back_order" value="
-            <?php
-                $Consulta_Grafica_Back_Order ="SELECT SUM( T1.[OpenQty] *  T1.[Price]  ) as Total FROM ORDR T0  INNER JOIN RDR1 T1 ON T0.DocEntry = T1.DocEntry INNER JOIN OSLP T2 ON T0.SlpCode = T2.SlpCode WHERE T0.[DocDate] >= '2013' AND  T2.[U_CODIGO_USA]  = ".$_SESSION['Usuario_Actual']." group by month(T0.[DocDate]) ORDER BY month(T0.[DocDate])";
-                $Resultado_Consulta_Grafica_Back_Order = odbc_exec($Conexion_SQL, $Consulta_Grafica_Back_Order);
-                while ($bac = odbc_fetch_array($Resultado_Consulta_Grafica_Back_Order)) {
-                  foreach ($bac as $meses_bac) {
-                    echo $meses_bac.",";
-                  }
+              }
+          ?>
+        ">
+        <input type="text" id="campos_back_order" value="
+          <?php
+              $Consulta_Grafica_Back_Order ="SELECT SUM( T1.[OpenQty] *  T1.[Price]  ) as Total FROM ORDR T0  INNER JOIN RDR1 T1 ON T0.DocEntry = T1.DocEntry INNER JOIN OSLP T2 ON T0.SlpCode = T2.SlpCode WHERE T0.[DocDate] >= '2013' AND  T2.[U_CODIGO_USA]  = ".$_SESSION['Usuario_Actual']." group by month(T0.[DocDate]) ORDER BY month(T0.[DocDate])";
+              $Resultado_Consulta_Grafica_Back_Order = odbc_exec($Conexion_SQL, $Consulta_Grafica_Back_Order);
+              while ($bac = odbc_fetch_array($Resultado_Consulta_Grafica_Back_Order)) {
+                foreach ($bac as $meses_bac) {
+                  echo $meses_bac.",";
                 }
-            ?>
-          ">
-          <!--/Inputs ocultos para la consulta de los valores de las graficas-->
-      </div>
+              }
+          ?>
+        ">
+        <!--/Inputs ocultos para la consulta de los valores de las graficas-->
     </div>
     <div class="col s12 m3">
           <ul class="collapsible" data-collapsible="accordion">
@@ -304,5 +302,5 @@ require('../assets/dashboard/header.php'); ?>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
   <script type="text/javascript" src="../js/master.js"></script>
-  <script src="js/material-charts.js"></script>
+    <script type="text/javascript" src="../js/material-charts.js"></script>
 </html>
--- js/master.js
+++ js/master.js
@@ -41,25 +41,22 @@ $(document).ready(function(){
            var Valores_Back_Order = document.getElementById("campos_back_order").innerHTML=Campos_Back_Order;
            var Mes_Back_Order = Valores_Back_Order.split(",");   //Se divide la cadena en partes para poder pasarlos a Float
 
-           var inicial = {
-             labels : ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],
-             datasets : [
-               {
-                 fillColor : "#4CAF50", //Color principal de las barras
-                 strokeColor : "rgba(220,220,220,0.8)",
-                 highlightFill: "#00796B",
-                 highlightStroke: "rgba(220,220,220,1)",
-                 data : [parseFloat(Mes_Facturacion[0]),parseFloat(Mes_Facturacion[1]),parseFloat(Mes_Facturacion[2]),parseFloat(Mes_Facturacion[3]),parseFloat(Mes_Facturacion[4]),parseFloat(Mes_Facturacion[5]),parseFloat(Mes_Facturacion[6]),parseFloat(Mes_Facturacion[7]),parseFloat(Mes_Facturacion[8]),parseFloat(Mes_Facturacion[9]),parseFloat(Mes_Facturacion[10]),parseFloat(Mes_Facturacion[11])]
-                   //Los valores dentro de "data", son los que se obtienen con el split() de la variable Valores_Facturacion
-                   //Deben convertirse a Float para respetar el punto decimal, no pueden ir campos tipo String dentro
-               }
-             ]
-           }
+           var exampleBarChartData = {
+             "datasets": {
+               "values": [100,200,300,400,500,600,700,800,900,1000,1100,1200],
+               "labels": ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"
+               ],
+               "color": "green"
+             },
+             "title": "Facturas de Clientes",
+             "noY": true,
+             "height": "400px",
+             "width": "980px",
+             "background": "#FFFFFF",
+             "shadowDepth": "1"
+           };
 
-           var ctx = document.getElementById("canvas").getContext("2d");
-           window.myBar = new Chart(ctx).Bar(inicial, {
-             responsive : true
-           });
+           MaterialCharts.bar("#barra", exampleBarChartData)
 
            //Funcion del botón Facturas de Clientes
            $('#facturas_clientes').on('click', function() {
