e2b8734e9c6b20011cd3a43105ca82c0eebaae16
Chubuntu Arciniega <arciniega.chubuntu@gmail.com>
2016-01-19 12:55:44 -0700

    Información superior index ventas

--- dashboard/index.php
+++ dashboard/index.php
@@ -8,8 +8,36 @@ require('../assets/dashboard/header.php'); ?>
 <!DOCTYPE html>
 <html>
   <body>
+    <!--Numeros Top-->
+    <div class="row" <?php if($_SESSION['Rango'] != 3){ echo "style='display: none;'";} ?>>
+      <div class="col m3 s12">
+        <div class="card-panel z-depth-3">
+            <h5 style="margin-top:-10px; "><i class="material-icons left teal lighten-1 white-text" style="font-size:230%; border-radius:5px;">attach_money</i><?php echo $facturas; ?></h5>
+            <p style="margin-bottom:-20px; margin-left:30%;">Total de Facturas</p>
+        </div>
+      </div>
+      <div class="col m3 s12">
+        <div class="card-panel z-depth-3">
+            <h5 style="margin-top:-10px; "><i class="material-icons left red lighten-1 white-text" style="font-size:230%; border-radius:5px;">attach_money</i><?php echo $ordenes; ?></h5>
+            <p style="margin-bottom:-20px; margin-left:30%;">Total de Ordenes</p>
+        </div>
+      </div>
+      <div class="col m3 s12">
+        <div class="card-panel z-depth-3">
+            <h5 style="margin-top:-10px; "><i class="material-icons left blue lighten-1 white-text" style="font-size:230%; border-radius:5px;">attach_money</i><?php echo $ofertas; ?></h5>
+            <p style="margin-bottom:-20px; margin-left:30%;">Total de Ofertas</p>
+        </div>
+      </div>
+      <div class="col m3 s12">
+        <div class="card-panel z-depth-3">
+          <h5 style="margin-top:-10px; "><i class="material-icons left purple lighten-1 white-text" style="font-size:230%; border-radius:5px;">attach_money</i><?php echo $back; ?></h5>
+            <p style="margin-bottom:-20px; margin-left:30%;">Total Back Order</p>
+        </div>
+      </div>
+    </div>
+    <!--/Numeros Top-->
   <!--Gráfica-->
-  <div class="row" style="display:none;">
+  <div class="row" <?php if($_SESSION['Rango'] != 3){ echo "style='display: none;'";} ?>>
     <div class="col s12 m9" id="ocultar">
       <div class="card-panel z-depth-3">
         <h5 id="Titulo_Grafica">Facturas de Clientes</h5>
@@ -191,8 +219,8 @@ require('../assets/dashboard/header.php'); ?>
   <!--/Rankings-->
   <!-- Comedor -->
   <div class="row">
-    <div  <?php if($_SESSION['Rango'] >= 4){ echo "class='col m9 s12'";}else{echo "class='col m12 s12'";} ?>>
-      <div class="card medium">
+    <div  <?php if($_SESSION['Rango'] == 3){ echo "style='display: none;'";} ?> >
+      <div class="card small">
             <div class="card-image">
               <img src="../img/avisos.png" height="992px" width="1200px">
               <span class="card-title">Nueva Versión AIMEX</span>
