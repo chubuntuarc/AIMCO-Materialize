67c643d66a90bdd63d6730945427063e29aae7e0
Chubuntu Arciniega <arciniega.chubuntu@gmail.com>
2016-02-08 16:34:22 -0700

    Acceso rapido

--- assets/dashboard/acceso_rapido.php
+++ assets/dashboard/acceso_rapido.php
@@ -9,10 +9,10 @@
     <a class=" grey-text text-lighten-4 right " href="#modal2">Contacto</a>
     <li><a class="btn-floating tooltipped red" data-position="top" data-delay="50" data-tooltip="Servicio"><i class="material-icons">build</i></a></li>
     <li><a class="modal-trigger btn-floating tooltipped black" data-position="top" data-delay="50" data-tooltip="Inventario" href="#modal_inventario"><i class="material-icons">assignment</i></a></li>
-    <li><a class="btn-floating tooltipped green" data-position="top" data-delay="50" data-tooltip="Contacto"><i class="material-icons">account_circle</i></a></li>
-    <li><a class="btn-floating tooltipped blue" data-position="top" data-delay="50" data-tooltip="Recordatorio"><i class="material-icons">bookmark_border</i></a></li>
-    <li><a class="btn-floating tooltipped yellow darken-3" data-position="top" data-delay="50" data-tooltip="Nota"><i class="material-icons">border_color</i></a></li>
-    <li><a class="btn-floating tooltipped purple" data-position="top" data-delay="50" data-tooltip="Correo"><i class="material-icons">mail</i></a></li>
+    <!--<li><a class="btn-floating tooltipped green" data-position="top" data-delay="50" data-tooltip="Contacto"><i class="material-icons">account_circle</i></a></li>-->
+    <!--<li><a class="btn-floating tooltipped blue" data-position="top" data-delay="50" data-tooltip="Recordatorio"><i class="material-icons">bookmark_border</i></a></li>-->
+    <!--<li><a class="btn-floating tooltipped yellow darken-3" data-position="top" data-delay="50" data-tooltip="Nota"><i class="material-icons">border_color</i></a></li>-->
+    <!--<li><a class="btn-floating tooltipped purple" data-position="top" data-delay="50" data-tooltip="Correo"><i class="material-icons">mail</i></a></li>-->
     <li><a class="btn-floating tooltipped cyan" data-position="top" data-delay="50" data-tooltip="Reporte"><i class="material-icons">mode_comment</i></a></li>
   </ul>
 </div>
